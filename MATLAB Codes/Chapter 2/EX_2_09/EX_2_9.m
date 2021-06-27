df6ebd26077f3f30b77a802a3c1774dfe6c0bc1c
mohsen-azimi <azimi.eng@gmail.com>
2021-06-27 15:09:37 -0700

    Chapter 2 updated

---
+++ MATLAB Codes/Chapter 2/EX_2_09/EX_2_9.m
@@ -0,0 +1,36 @@
+%% Example 2-9: Plot Displacemetn Response of An Underdamped SDOF with Different Damping Ratios 
+
+clear; close all; clc;
+%% Parameters
+wn  = 5;  % Natural frequency [rad/s]
+
+x0  = 0;                % Initial displacement
+v0  = 60;               % Initial velocity [cm/s]
+t0  = 0;                % Initial time
+deltat = 0.01;          % Time step
+tf  = 6;                % Final time
+t   = [t0:deltat:tf];
+
+%% Calculate Response & Plot
+figure(1); set(figure(1), 'Position', [200   100   883   400])
+           hold on; grid on; box on;
+
+           Legends = {'\zeta = 0.05';'\zeta = 0.10';'\zeta = 0.20'};
+           Lines   = {'-k';          '--k';         ':k'};
+
+i = 0;
+for zeta = [0.05,0.1,0.2]
+    wd = sqrt(1-zeta^2)*wn; % Damped frequency
+    x  = exp(-zeta*wn*t).*(((zeta*wn*x0+v0)/wd)*sin(wd*t)+ x0*cos(wd*t));
+
+        i = i+1;
+        plot(t,x,Lines{i},'LineWidth',2);  ylim([-15 15]);
+end
+
+        set(gca, 'LineWidth',1, 'FontWeight','normal', 'FontName','Times New Roman', 'FontSize',10)               
+        xlabel('Time (s)', 'fontsize',12, 'fontname','Times New Roman','FontWeight','Bold')
+        ylabel('Displacement (cm)', 'fontsize',12, 'fontname','Times New Roman','FontWeight','Bold')
+        legend(Legends,'location','northeast', 'orientation','horizontal')
+
+%% Print
+print('EX_2_9','-dpng')
